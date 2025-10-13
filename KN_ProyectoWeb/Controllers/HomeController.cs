b758fd1483b594dabe491d41e59b422c6553c1d4
Gabriela <mjimenez40125@ufide.ac.cr>
2025-10-12 19:09:49 -0600

    Semana 3 Final

--- KN_ProyectoWeb/Controllers/HomeController.cs
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -1,25 +1,58 @@
-﻿using System.Web.Mvc;
+﻿using KN_ProyectoWeb.Models;
+using System.Web.Mvc;
 
 namespace KN_ProyectoWeb.Controllers
 {
     public class HomeController : Controller
     {
+        #region Iniciar Sesión
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            /*Progra para ir a iniciar sesión en la BD*/
+
+            return RedirectToAction("Principal", "Home");
+        }
+        #endregion
+
+        #region Registrarse
+
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            /*Progra para ir a guardar el usuario en la BD*/
+            return View();
+        }
+        #endregion
+
+        #region Recuperar Contraseña
+
+        [HttpGet]
         public ActionResult RecuperarAcceso()
         {
             return View();
         }
 
-
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario usuario)
+        {
+            /*Validar si el usuario existe, generar una clave temporal, enviarle esa clave al usuario*/
+            return View();
+        }
+        #endregion
 
 
         public ActionResult Principal()
